c944b114aec8e18516ee85ec914cd21150fbe896
dudismiranda <mirandadudis@gmail.com>
2023-08-25 21:17:55 -0300

    Atividade Hello Word e Atividades de Variáveis

---
+++ 02. Variáveis/02.1 Calculadora de IMC/index.js
@@ -0,0 +1,7 @@
+const peso = 76;
+const altura = 1.79;
+let imc;
+
+imc = peso / (altura * altura);
+
+console.log(imc);
---
+++ 02. Variáveis/02.2 Fahrenheit x Celsius/index.js
@@ -0,0 +1,7 @@
+let fahrenheit;
+let celsius;
+
+fahrenheit = 73;
+celsius = (fahrenheit - 32) * (5 / 9);
+
+console.log(celsius + "°C");
